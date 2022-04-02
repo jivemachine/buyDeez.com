a181f1d08c48ae3978a6b36438a05a1556fa76f7
jivemachine <the.jeremy.cobb@gmail.com>
2022-04-02 17:43:25 -0500

    fixed spelling issue in settings.py

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'EmployeeApp.apps.Employeeapp.Config',
+    'EmployeeApp.apps.EmployeeappConfig',
     'rest_framework'
 ]
 
