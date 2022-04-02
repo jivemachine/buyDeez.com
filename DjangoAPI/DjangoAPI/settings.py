d0342635555334b2f2ed40c48ebb62cb5a16bbaf
jivemachine <the.jeremy.cobb@gmail.com>
2022-04-02 17:42:00 -0500

    added employee apps config and rest framework to installed apps in settings.py

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'EmployeeApp.apps.Employeeapp.Config',
+    'rest_framework'
 ]
 
 CORS_ORGIN_ALLOW_ALL = True
